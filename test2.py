e43418588856160c1a64c964c2d19526b828f6be
mohamad <mohamad97mj@gmail.com>
2020-12-24 15:23:36 +0330

    test2 updated

--- test2.py
+++ test2.py
@@ -10,7 +10,7 @@ class SMSGateway(Enum):
 mobile_no = '09059242876'
 message = 'hello, world!'
 settings = Settings(strict=False, force_https=False, xml_huge_tree=True)
-client = Client('SendSMS.xml', settings=settings)
+client = Client('test.wsdl', settings=settings)
 with client.settings(raw_response=True):
     response = client.service.SendSMS(mobile_no, message, SMSGateway.ADVERTISEMENT, "Mojahed sms service")
     f = open("myfile2.txt", "w")
