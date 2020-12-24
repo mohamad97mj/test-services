b3da503446f107cb4ddc593fd351df2e7e616115
mohamad <mohamad97mj@gmail.com>
2020-12-24 14:24:33 +0330

    tset2

---
+++ test2.py
@@ -0,0 +1,18 @@
+from zeep import Client, Settings
+from enum import Enum
+
+
+class SMSGateway(Enum):
+    ADVERTISEMENT = 1,
+    PAYMENT = 2,
+
+
+mobile_no = '09059242876'
+message = 'hello, world!'
+settings = Settings(strict=False, force_https=False, xml_huge_tree=True)
+client = Client('SendSMS.xml', settings=settings)
+with client.settings(raw_response=True):
+    response = client.service.SendSMS(mobile_no, message, SMSGateway.ADVERTISEMENT, "Mojahed sms service")
+    f = open("myfile2.txt", "w")
+    f.write(str(result))
+    f.close()
