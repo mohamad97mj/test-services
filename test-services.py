4540c01723da5ce0eb0d4c26db511d1e8d1a6870
mohamad <mohamad97mj@gmail.com>
2020-12-24 15:10:39 +0330

    test

--- test-services.py
+++ test-services.py
@@ -17,7 +17,7 @@ client = Client('SendSMS.xml', plugins=[history])
 with client.settings(force_https=False, raw_response=True):
     service = client.create_service(
         '{http://tempuri.org/}SendSMSSoap',
-        'http://10.0.32.43:80')
+        'http://10.0.32.43:80/SendSMS.asmx')
 
     result = service.SendSMS(mobile_no, message, SMSGateway.ADVERTISEMENT, "Mojahed sms service")
 
