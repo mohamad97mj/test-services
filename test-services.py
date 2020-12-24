68b1906a0f8bf5e8db52f470f7e1b15a1c8daf79
mohamad <mohamad97mj@gmail.com>
2020-12-24 13:30:00 +0330

    to test

---
+++ test-services.py
@@ -0,0 +1,29 @@
+from zeep import Client
+from zeep.plugins import HistoryPlugin
+from lxml import etree
+from enum import Enum
+
+
+class SMSGateway(Enum):
+    ADVERTISEMENT = 1,
+    PAYMENT = 2,
+
+
+history = HistoryPlugin()
+mobile_no = '09059242876'
+message = 'hello, world!'
+client = Client('SendSMS.xml', plugins=[history])
+
+with client.settings(force_https=False):
+    service = client.create_service(
+        '{http://tempuri.org}SendSMSSoap',
+        'https://10.0.32.43:80/SendSMS.asmx')
+
+    result = client.service.SendSMS(mobile_no, message, SMSGateway.ADVERTISEMENT, "Mojahed sms service")
+
+    your_pretty_xml = etree.tostring(
+        history.last_received["envelope"], encoding="unicode", pretty_print=True)
+
+    f = open("myfile.txt", "w")
+    f.write(str(your_pretty_xml))
+    f.close()
